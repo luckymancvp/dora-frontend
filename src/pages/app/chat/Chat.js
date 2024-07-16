a71e275b320f0dbcd9a1d00743e3df55cb38efa9
Cuong Le Manh <cuonglm@pamoteam.com>
2024-07-16 10:35:56 +0700

    up

--- src/pages/app/chat/Chat.js
+++ src/pages/app/chat/Chat.js
@@ -131,7 +131,7 @@ const Chat = ({
             <div className="nk-chat-aside-head">
               <div className="nk-chat-aside-user">
                 <UncontrolledDropdown>
-                  <DropdownToggle tag="a" className="dropdown-toggle dropdown-indicator">
+                  <DropdownToggle tag="a" className="dropdown-toggle dropdown-indicator" style={{ marginRight: "30px"}}>
                     <UserAvatar image={User}></UserAvatar>
                     <div className="title">{mainTab}</div>
                   </DropdownToggle>
